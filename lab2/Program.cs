52d49f431d16fb41f28aacafa8e69785400f22e7
BuildTools <unconfigured@null.spigotmc.org>
2021-04-10 07:49:56 +0500

    добавил регулярные для всего

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -11,7 +11,7 @@ namespace lab2
 
     enum TokenType
     {
-        Identifier, Punctuation, Number, String, WhiteSpace, Operator, Comment
+        Identifier, Number, Operator, String, Punctuation, WhiteSpace, Comment
     }
 
     class Token
@@ -37,24 +37,42 @@ namespace lab2
     {
         static IEnumerable<Token> GetTokens(string inputText)
         {
-            var regex = new Regex(@"(?<Identifier>[a-zA-Z_][a-zA-Z_0-9]*)", RegexOptions.IgnorePatternWhitespace);
+            var regex = new Regex(@"(?<Identifier>[a-zA-Z_][a-zA-Z_0-9]*)|
+                                    (?<Number>[0-9]+)|
+                                    (?<Operator>\!\=|\+\=|\=\>|\=\<|\+|\=|\-|\*)|
+                                    (?<String>\@""(""""|[^""])*"")|
+                                    (?<Punctuation>\.|\(|\)|\{|\}|\[|\]|\;|\<|\>|\,|\:)|
+                                    (?<WhiteSpace>[\ \t\r\n]+)|
+                                    (?<Comment>(\/\/)(.+?)(?=[\n\r]|\*\)))", RegexOptions.IgnorePatternWhitespace);
             var matches = regex.GetMatch(inputText);
-            foreach(var match in matches)
+            foreach (var match in matches)
             {
                 if (match.Groups[@"Identifier"].Success)
                     yield return new Token(match.Value, TokenType.Identifier);
+                else if (match.Groups[@"Number"].Success)
+                    yield return new Token(match.Value, TokenType.Number);
+                else if (match.Groups[@"Operator"].Success)
+                    yield return new Token(match.Value, TokenType.Operator);
+                else if (match.Groups[@"String"].Success)
+                    yield return new Token(match.Value, TokenType.String);
+                else if (match.Groups[@"Punctuation"].Success)
+                    yield return new Token(match.Value, TokenType.Punctuation);
+                else if (match.Groups[@"WhiteSpace"].Success)
+                    yield return new Token(match.Value, TokenType.WhiteSpace);
+                else if (match.Groups[@"Comment"].Success)
+                    yield return new Token(match.Value, TokenType.Comment);
             }
         }
-        static void Main(string[] args)
+        static void Main(string[] args)//COMM 123
         {
             var inputText = File.ReadAllText(@"../../Program.cs");
             var sb = new StringBuilder();
-            foreach(var token in GetTokens(inputText))
+            foreach (var token in GetTokens(inputText))
             {
                 sb.Append(token.Lexeme);
             }
             Console.WriteLine(sb.ToString());
-            File.WriteAllText(@"../../../lab2Out/Program.cs",sb.ToString());
+            File.WriteAllText(@"../../../lab2Out/Program.cs", sb.ToString());
         }
     }
 }
--- lab2Out/Program.cs
+++ lab2Out/Program.cs
@@ -1,1 +1,78 @@
-usingSystemusingSystemCollectionsGenericusingSystemLinqusingSystemTextusingSystemThreadingTasksusingSystemIOusingSystemTextRegularExpressionsnamespacelab2enumTokenTypeIdentifierPunctuationNumberStringWhiteSpaceOperatorCommentclassTokenpublicstringLexemegetpublicTokenTypeTypegetpublicTokenstringlexemeTokenTypetypeLexemelexemeTypetypestaticclassMatchTokenpublicstaticIEnumerableMatchGetMatchthisRegexregexstringinputTextforvarmatchregexMatchinputTextmatchSuccessmatchmatchNextMatchyieldreturnmatchclassProgramstaticIEnumerableTokenGetTokensstringinputTextvarregexnewRegexIdentifierazAZ_azAZ_0RegexOptionsIgnorePatternWhitespacevarmatchesregexGetMatchinputTextforeachvarmatchinmatchesifmatchGroupsIdentifierSuccessyieldreturnnewTokenmatchValueTokenTypeIdentifierstaticvoidMainstringargsvarinputTextFileReadAllTextProgramcsvarsbnewStringBuilderforeachvartokeninGetTokensinputTextsbAppendtokenLexemeConsoleWriteLinesbToStringFileWriteAllTextlab2OutProgramcssbToString
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lab2
+{
+
+    enum TokenType
+    {
+        Identifier, Number, Operator, String, Punctuation, WhiteSpace, Comment
+    }
+
+    class Token
+    {
+        public string Lexeme { get; }
+        public TokenType Type { get; }
+
+        public Token(string lexeme, TokenType type)
+        {
+            Lexeme = lexeme;
+            Type = type;
+        }
+    }
+    static class MatchToken
+    {
+        public static IEnumerable<Match> GetMatch(this Regex regex, string inputText)
+        {
+            for (var match = regex.Match(inputText); match.Success; match = match.NextMatch())
+                yield return match;
+        }
+    }
+    class Program
+    {
+        static IEnumerable<Token> GetTokens(string inputText)
+        {
+            var regex = new Regex(@"(?<Identifier>[a-zA-Z_][a-zA-Z_0-9]*)|
+                                    (?<Number>[0-9]+)|
+                                    (?<Operator>\!\=|\+\=|\=\>|\=\<|\+|\=|\-|\*)|
+                                    (?<String>\@""(""""|[^""])*"")|
+                                    (?<Punctuation>\.|\(|\)|\{|\}|\[|\]|\;|\<|\>|\,|\:)|
+                                    (?<WhiteSpace>[\ \t\r\n]+)|
+                                    (?<Comment>(\/\/)(.+?)(?=[\n\r]|\*\)))", RegexOptions.IgnorePatternWhitespace);
+            var matches = regex.GetMatch(inputText);
+            foreach (var match in matches)
+            {
+                if (match.Groups[@"Identifier"].Success)
+                    yield return new Token(match.Value, TokenType.Identifier);
+                else if (match.Groups[@"Number"].Success)
+                    yield return new Token(match.Value, TokenType.Number);
+                else if (match.Groups[@"Operator"].Success)
+                    yield return new Token(match.Value, TokenType.Operator);
+                else if (match.Groups[@"String"].Success)
+                    yield return new Token(match.Value, TokenType.String);
+                else if (match.Groups[@"Punctuation"].Success)
+                    yield return new Token(match.Value, TokenType.Punctuation);
+                else if (match.Groups[@"WhiteSpace"].Success)
+                    yield return new Token(match.Value, TokenType.WhiteSpace);
+                else if (match.Groups[@"Comment"].Success)
+                    yield return new Token(match.Value, TokenType.Comment);
+            }
+        }
+        static void Main(string[] args)//COMM 123
+        {
+            var inputText = File.ReadAllText(@"../../Program.cs");
+            var sb = new StringBuilder();
+            foreach (var token in GetTokens(inputText))
+            {
+                sb.Append(token.Lexeme);
+            }
+            Console.WriteLine(sb.ToString());
+            File.WriteAllText(@"../../../lab2Out/Program.cs", sb.ToString());
+        }
+    }
+}
